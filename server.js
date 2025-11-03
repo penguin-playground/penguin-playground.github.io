a93f803df1763f2e01e91dbe827214bae38c769f
Gabe <gabriel.kravets@outlook.com>
2025-11-02 22:45:07 -0500

    started server

--- server.js
+++ server.js
@@ -13,15 +13,33 @@ function buildAvlTree(){
             .on('data', (data) => results.push(data))
             .on('end', () => {
                 const gameObjects = CSVResultsToObject(results); //stores gameObjList
-                for (const game in gameObjects){
+                for (const game of gameObjects){
                     avlTree.insertNode(avlTree.root, game)
                 }
                 resolve(avlTree); //waits till tree built
             })
             .on('error', reject)
     });
+    return buildTree;
 }
 
 async function startServer(){
     //need to start a server and listen to api requests
+    try {
+        const avlTree = await buildAvlTree(); // wait for the tree to be built
+        app.get('/api/search', (req,res) => {
+            const filters = req.query //.../api/search?genre=Sports&platform=Wii&year=2009 becomes an obj: {genre: 'Sports',platform: 'Wii',year: '2009'}
+            const result = avlTree.BFS(filters)
+            res.json({
+                result
+            });
+        })
+        app.listen(port , ()=>{
+            console.log("Server running")
+        });
+    } catch (error){
+        console.log("Something went wrong")
+    }
 }
+
+startServer();
